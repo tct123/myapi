518672ee26f5bb7c57983e95c4ae1ae39b05d75f
tct123 <42028373+tct123@users.noreply.github.com>
2024-02-10 02:37:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import fastapi as fa
+import pydantic
+
+mylist = [
+    {"name": "Computer1", "price_in_euro": 100, "typ": "hardware"},
+    {"name": "Computer2", "price_in_euro": 100, "typ": "hardware"},
+    {"name": "Computer3", "price_in_euro": 100, "typ": "hardware"},
+    {"name": "Computer4", "price_in_euro": 100, "typ": "hardware"},
+]
+
+app = fa.FastAPI()
+
+
+@app.get("/items")
+async def hello():
+    return mylist
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int):
+    if item_id >= len(mylist):
+        return [{"name": "toolong", "price_in_euro": 0, "typ": "toolong"}]
+    else:
+        return mylist[item_id]
